23ae0c0db78509942a730c6a689dab3266f6b736
AndreaMancuso02 <andremancu4@gmail.com>
2025-07-02 09:05:38 +0200

    Add files via upload

---
+++ 04-events/js/script01.js
@@ -0,0 +1,20 @@
+function salutami(){
+    document.querySelector("#demo").innerHTML = "<h1>Ciao Dario</h1>"
+}
+
+let btn2 = document.querySelector("#btn2");
+btn2.onclick = salutami;
+// in disuso
+
+let btn3 = document.querySelector("#btn3");
+btn3.addEventListener("click", salutami);
+
+let demo = document.querySelector("#demo");
+demo.addEventListener("mouseenter", function(){
+    demo.setAttribute("style", "background-color: blue");
+})
+demo.addEventListener("mouseleeve", function(){
+    demo.removeAttribute("style");
+})
+
+
